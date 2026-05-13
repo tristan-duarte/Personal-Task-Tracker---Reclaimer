89d220be3f2c4a593ffdaa36df0ffde9e6e6fb56
trist <tduar10@wgu.edu>
2026-05-13 16:18:09 -0700

    Initial Spring Boot project steup with MySQL, JPA, validation

---
+++ src/main/java/com/tduar10/personaltasktracker/PersonalTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tduar10.personaltasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalTaskTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tduar10/personaltasktracker/PersonalTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tduar10.personaltasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
