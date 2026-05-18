9c6278dadc71b4e2c20f6ccc252298806ae5e5ec
tristan-duarte <tristanduarte473@gmail.com>
2026-05-17 20:50:34 -0700

    Add TaskController with REST endpoints, exception handler, and HTTP test requests

--- src/main/java/com/tduar10/personaltasktracker/task/TaskController.java
+++ src/main/java/com/tduar10/personaltasktracker/task/TaskController.java
@@ -1,7 +1,48 @@
 package com.tduar10.personaltasktracker.task;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> findAll() {
+        return taskService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Task findById(@PathVariable long id) {
+        return taskService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task create(@RequestBody Task task) {
+        return taskService.create(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task update(@PathVariable Long id, @Valid @RequestBody Task task) {
+        return taskService.update(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        taskService.delete(id);
+    }
+
+
+
 }
